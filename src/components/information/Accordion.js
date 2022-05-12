c8e2d2f0cf3ac13cee6cd46838332144e7017b3b
Hafid Ali Mustaqim <hafidalimustaqim13@gmail.com>
2022-05-12 10:14:44 +0700

    fixing accordion on footer

--- src/components/information/Accordion.js
+++ src/components/information/Accordion.js
@@ -1,16 +1,37 @@
-import {useState} from "react";
+import {useEffect, useState} from "react";
 import {ChevronDownIcon} from "@heroicons/react/solid";
 
 const Accordion = ({title, content}) => {
     const [active, setActive] = useState(false)
+    const [show, setShow] = useState(false)
+
+    const changeWindow = () =>{
+        if (window.innerWidth >= 768){
+            setActive(true)
+            setShow(false)
+        }else{
+            setActive(false)
+            setShow(true)
+        }
+    }
+
+    useEffect(()=>{
+        window.addEventListener("resize", changeWindow)
+    })
+    const handleOnClick = () =>{
+        if (!window.matchMedia("(max-width: 768px").matches){
+            return;
+        }
+        setActive(!active)
+    }
 
     return(
         <div className={"col"}>
-            <div className={"flex justify-between"} onClick={()=>setActive(!active)}>
+            <div className={"flex justify-between"} onClick={()=>handleOnClick()}>
                 <div className={""}><h5 className={"font-semibold"}>{title}</h5></div>
-                <ChevronDownIcon width={"24"}/>
+                {show ? <ChevronDownIcon width={"24"}/> : ""}
             </div>
-            {active && <p className={"text-sm"}>{content}</p> }
+            {active && <p className={"text-sm"} dangerouslySetInnerHTML={{__html: content }}></p> }
         </div>
     )
 
